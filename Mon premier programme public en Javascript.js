bddff45f80c0935fc93958517b4a68d659bde868
Bixente13 <115988042+Bixente13@users.noreply.github.com>
2025-05-04 15:42:02 +0200

    Create Mon premier programme public en Javascript.js
    Il s'agit de mon tout premier programme public en Javascript. Il permet de faire choisir à l'utilisateur entre créer un tableau (il faut qu'il entre la taille du tableau, puis le programme crée le tableau, puis ensuite affiche la moyenne, la variance et l'écart-type), et utiliser les fonctions usuelles (puissance, cosinus, sinus, tangente, sinus hyperbolique, cosinus hyperbolique, tangente hyperbolique, exponentielle, logarithme népérien, logarithme de base réelle a),  appliquées sur un nombre réel x.

---
+++ Mon premier programme public en Javascript.js
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head> 
+<title>Mon sixième Programme Javascript</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<script>
+function CréerUnTableau() {
+    let tableau = []; // On initialise le tableau vide
+    let taille = parseInt(prompt("Entrez la taille du tableau :")); // On demande à l'utilisateur la taille du tableau
+    for(let i = 0; i < taille; i++) { // Pour chaque case du tableau
+        if(i == 0) { tableau[i]=parseFloat(prompt("Entrez la 1ère valeur de du tableau :")); } // On demande à l'utilisateur de remplir la première case
+        
+        else { tableau[i] = parseFloat(prompt("Entrez la "+ (i+1) +"ème valeur du tableau :")); }
+    } // On remplit les autres cases avec des nombres aléatoires entre 0 et 100
+        return tableau;  // On retourne le tableau rempli
+}
+
+function calculMoyenne(tableau) {
+    let somme = 0;
+    for (let i = 0; i < tableau.length; i++) {
+        somme += tableau[i];
+    }
+    return somme / tableau.length;
+}
+
+function calculVariance(tableau) {
+    let moyenne = calculMoyenne(tableau);
+    let sommeCarres = 0;
+    for (let i = 0; i < tableau.length; i++) {
+        sommeCarres += Math.pow(tableau[i] - moyenne, 2);
+    }
+    return sommeCarres / tableau.length;
+}
+
+function calculEcartType(tableau) {
+    return Math.sqrt(calculVariance(tableau));
+}
+
+function CalculFactorielle(n) {
+    if (n === 0 || n === 1) { // Si n est égal à 0 ou 1, on retourne 1
+        return 1;
+    } 
+    else 
+    { 
+        let produit = 1; // On initialise le produit à 1
+        for (i = n; i >0; i--) 
+            { // On initialise la valeur i à n et on décrémente i de 1 pour arriver jusqu'à 0
+            produit *= i; // On multiplie le produit par i
+            }
+            return produit; // On retourne le produit           
+    }
+}
+
+
+function CalculCoefficientBinomial(n, k) { 
+    if (k > n) { // Si k est supérieur à n, on retourne 0
+        return 0;
+    }   
+    if (k === 0 || k === n) { // Si k est égal à 0 ou n, on retourne 1
+        return 1;           
+    }
+    else return CalculFactorielle(n) / (CalculFactorielle(k) * CalculFactorielle(n - k)); // On retourne le coefficient binomial
+}
+
+function AfficherPuisance(x,n) {
+    alert("La puissance de " + x + " à la puissance " + n + " , autrement dit "+ x +"^"+ n + " , est : " + Math.pow(x,n) + "."); // On affiche la puissance de x à la puissance n
+}
+
+function AfficherExponentielle(x) {
+    alert("L'exponentielle de " + x + " , autrement dit exp(" + x + "), est : " + Math.exp(x) + "."); // On affiche l'exponentielle de x
+}
+
+function AfficherCosinus(x) {
+    alert("Le cosinus de " + x + " , autrement dit cos("+ x + "), est :" + Math.cos(x) +"."); // On affiche le cosinus de x
+}
+
+function AfficherSinus(x) {
+    alert("Le sinus de " + x + " , autrement dit cos(" + x + "), est : "+ Math.sin(x)+"."); // On affiche le sinus de x
+}
+
+function AfficherTangente(x) {
+   alert("La tangente de " + x + " , autrement dit tan(" + x + "), est :"); // On affiche la tangente de x     
+}
+
+function AfficherCosinusHyperbolique(x) {
+    alert("Le cosinus hyperbolique de " + x + " , autrement dit cosh(" + x + "), est : " + Math.cosh(x) + "."); // On affiche le cosinus hyperbolique de x
+}
+
+function AfficherSinusHyperbolique(x) {
+    alert("Le sinus hyperbolique de " + x + " , autrement dit sinh(" + x + "), est : " + Math.sinh(x) + "."); // On affiche le sinus hyperbolique de x  
+}
+
+function AfficherTangenteHyperbolique(x) {
+    alert("La tangente hyperbolique de " + x + " , autrement dit tanh(" + x + "), est : " + Math.tanh(x) + "."); // On affiche la tangente hyperbolique de x
+}
+
+function AfficherLogarithmeNépérien(x){
+    
+    if(x <= 0) { // Si x est inférieur ou égal à 0, on affiche un message d'erreur
+        alert("Erreur : le logarithme népérien n'est pas défini pour les nombres inférieurs ou égaux à 0.");
+    }
+    else { // Sinon, on affiche le logarithme népérien de x
+        alert("Le logarithme népérien de " + x + ", autrement dit ln("+ x + "),  est : " + Math.log(x) + ".");
+    }
+}
+
+function AfficherLogarithmeBase(x,a) {
+    if(x <= 0 || a <= 0 || a == 1) { // Si x est inférieur ou égal à 0, ou si a est inférieur ou égal à 0, ou si a est égal à 1, on affiche un message d'erreur
+        alert("Erreur : le logarithme de base " + a + " n'est pas défini pour les nombres inférieurs ou égaux à 0, ou pour la base 1.");
+    }
+    else { // Sinon, on affiche le logarithme de x à la base a
+        alert("Le logarithme de " + x + " à la base " + a + " est : " + Math.log(x) / Math.log(a) + ".");
+    }
+}
+function ChoixAfficherTCalculOuFonctionUsuelle() 
+{
+    alert("Choisissez une option entre: créer un tableau,  ou utiliser une des fonctions usuelles sur un nombre réel.");
+    let choix = prompt("Entrez le message \"créer tableau\" pour créer un tableau, ou le message \"utiliser fonction usuelle\" pout utiliser calcuer l'une des fonctions usuelles d'un nombre réel. "); // On demande à l'utilisateur de choisir une option
+    if(choix == "créer tableau") 
+    { 
+        alert("Maintenant,entrez les valeurs du tableau, et seront affichés par la suite la moyenne, la variance ainsi que l' écart-type du tableau"); // Si l'utilisateur choisit de créer un tableau
+        let tableau = CréerUnTableau(); // On crée le tableau
+        alert("Voici le tableau que vous avez créé : " + tableau + ". \n Pour info : \n La moyenne du tableau est : " + calculMoyenne(tableau) + ". \n La variance du tableau est : " + calculVariance(tableau) + ". \n L'écart-type du tableau est : " + calculEcartType(tableau) +" ." );// On affiche le tableau créé
+    }    
+    
+    else if(choix == "utiliser fonction usuelle") // Si l'utilisateur choisit d'utiliser une fonction usuelle sur un nombre réel
+    {
+        let x = parseFloat(prompt("Entrez un nombre réel x:")); // On demande à l'utilisateur de saisir un nombre réel x
+        let fonction = prompt("Choisissez une fonction parmi : puissance, exponentielle, cosinus, sinus, tangente, logarithme népérien, logarithe de base réelle a, cosinus hyperbolique, sinus hyperbolique, ou tangente hyperbolique."); // On demande à l'utilisateur de choisir une fonction
+        if(fonction == "puissance") { // Si l'utilisateur choisit la fonction puissance
+            let n = parseInt(prompt("Entrez un entier n :")); // On demande à l'utilisateur de saisir un entier n
+            AfficherPuisance(x,n); // On affiche la puissance de x à la puissance n
+        } 
+        else if(fonction == "exponentielle") { // Si l'utilisateur choisit la fonction exponentielle
+            AfficherExponentielle(x); // On affiche l'exponentielle de x
+        } 
+        else if(fonction == "cosinus") { // Si l'utilisateur choisit la fonction cosinus
+            AfficherCosinus(x); // On affiche le cosinus de x
+        } 
+        else if(fonction == "sinus") { // Si l'utilisateur choisit la fonction sinus
+            AfficherSinus(x); // On affiche le sinus de x
+        } 
+        else if(fonction == "tangente") { // Si l'utilisateur choisit la fonction tangente
+            AfficherTangente(x); // On affiche la tangente de x
+        } 
+        else if(fonction == "logarithme népérien") { // Si l'utilisateur choisit la fonction logarithme népérien
+            AfficherLogarithmeNépérien(x); // On affiche le logarithme népérien de x
+        }
+        else if(fonction == "logarithme de base réelle a") { // Si l'utilisateur choisit la fonction logarithme de base réelle a
+            let a = parseFloat(prompt("Entrez un nombre réel positif a :")); // On demande à l'utilisateur de saisir un nombre réel positif a
+            AfficherLogarithmeBase(x,a); // On affiche le logarithme de x à la base a
+        }   
+        else if(fonction == "cosinus hyperbolique") { // Si l'utilisateur choisit la fonction cosinus hyperbolique
+            AfficherCosinusHyperbolique(x); // On affiche le cosinus hyperbolique de x
+        } 
+        else if(onction == "sinus hyperbolique") { // Si l'utilisateur choisit la fonction sinus hyperbolique
+            AfficherSinusHyperbolique(x); // On affiche le sinus hyperbolique de x
+        } 
+        else if(fonction == "tangente hyperbolique") { // Si l'utilisateur choisit la fonction tangente hyperbolique
+            AfficherTangenteHyperbolique(x); // On affiche la tangente hyperbolique de x
+        } 
+        else {alert("Choix invalide !");} // Si le choix est invalide, on affiche un message d'erreur   
+    }
+    else {alert("Choix invalide !");} // Si le choix est invalide, on affiche un message d'erreur 
+}
+
+ChoixAfficherTCalculOuFonctionUsuelle(); // On appelle la fonction ChoixAfficherTCalculOuFonctionUsuelle pour exécuter le code
+
+</script>
+</body>
+</hml>
